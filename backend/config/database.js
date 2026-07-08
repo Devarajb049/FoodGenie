3a2d4dfb78c90c999a27041d41a5cf20e1b780de
Bhojanapu Deva Raj <bhojanapudevaraj@gmail.com>
2026-07-08 11:43:06 +0530

    Gracefully skip database connection on backend if DB_URI is missing

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
+  if (!process.env.DB_URI) {
+    console.warn("MongoDB Connection Skipped: DB_URI environment variable is not defined.");
+    return;
+  }
   try {
     const con = await mongoose.connect(process.env.DB_URI);
 
     console.log(`MongoDB Connected: ${con.connection.host}`);
   } catch (error) {
-    console.error(error);
+    console.error("MongoDB Connection Error:", error.message);
   }
 };
 
